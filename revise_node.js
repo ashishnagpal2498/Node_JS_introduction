5ac5efe4a85944f14e76ccc62d6fdbb5b2f65e10
ashish nagpal <ashishnagpal2498@gmail.com>
2019-02-26 18:26:24 +0530

    Revise Js

--- revise_node.js
+++ revise_node.js
@@ -1,5 +1,14 @@
 //JavaScript -
 //It is a prog lan - Interpreter - Web browsers , Nodejs
 //Functional and objects are two main constituents of the lang
+let obj = {
+    a:"g",
+    b :20
+};
+//Prototype inheritance , Closures , functions , IIFE ,
+console.log(obj);
+//Scope - Local Global - this pointer
+function f() {
+}
 
 //Revise node- on 23rd Feb -
