0b8302753424b642c02a510c360d341c539561c9
ashish nagpal <ashishnagpal2498@gmail.com>
2019-03-02 23:51:18 +0530

    Javascript- Arrays higher order

--- Intro_toNode.js
+++ Intro_toNode.js
@@ -12,37 +12,8 @@ console.log('hello');
 // let a = parseInt(process.argv[2]);
 // let b = parseInt(process.argv[3]);
 // console.log(a+b);
-let abcde = {
-    a:6
-}
-abcde.p = 99;
-console.log(abcde);
-console.log({}+[]);
-//If an empty object - toString -
-console.log(toString({}))
-delete abcde.a;
-console.log(abcde);
-document.cookie = [7];
-//Number ke Froms ;
-// let , NUMBER object -
-// let arg = "10";
-//SUBSTR
-let lmnstr = "abcdehiuyhabc";
-console.log(lmnstr.indexOf('a',4));
-console.log(lmnstr.length);
-console.log("strings"+lmnstr.substr(3,3))
-console.log(lmnstr.substring(3,4))
-//splice - 1st index - length -
-//Higher order functions -
+
 // let p = new Number(arg);
-
-//sum = add(a,b) ; return value sum , if we do sum = add - this creates a reference function
-//Closure - Scope of variables inside a function
-
-// Array sorting - lexicographical -
-
-//MAP FILTER REDUCE
-
 
 //Number as integer and object
 //Number . tosource
---
+++ JS_basics_3rdMarch.js
@@ -0,0 +1,30 @@
+let abcde = {
+    a:6
+}
+abcde.p = 99;
+console.log(abcde);
+console.log({}+[]);
+//If an empty object - toString -
+console.log(toString({}))
+delete abcde.a;
+console.log(abcde);
+document.cookie = [7];
+//Number ke Froms ;
+// let , NUMBER object -
+// let arg = "10";
+//SUBSTR
+let lmnstr = "abcdehiuyhabc";
+console.log(lmnstr.indexOf('a',4));
+console.log(lmnstr.length);
+console.log("strings"+lmnstr.substr(3,3))
+console.log(lmnstr.substring(3,4))
+//splice - 1st index - length -
+//Higher order functions -
+
+
+//sum = add(a,b) ; return value sum , if we do sum = add - this creates a reference function
+//Closure - Scope of variables inside a function
+
+// Array sorting - lexicographical -
+
+//MAP FILTER REDUCE
