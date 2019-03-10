f3720678d9ac3ca3f8ae9b677c9f0f21d076178b
ashish nagpal <ashishnagpal2498@gmail.com>
2019-03-11 01:36:05 +0530

    JSON OOPs

--- JSON_File/json_js.js
+++ JSON_File/json_js.js
@@ -1,6 +1,5 @@
-window.onload = function () {
-    let header = document.querySelector('header')
-    let section = document.querySelector('section');
+ var header = document.querySelector('header')
+    var section = document.querySelector('section');
 
 //    Json Object
     var RequestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
@@ -18,6 +17,49 @@ window.onload = function () {
         var superheros = request.response;
         //Functions which are taking Json data -
         populateHeader(superheros);
-        showHeros(superheros);
+        showHeroes(superheros);
+    }
+
+function populateHeader(jsonObj) {
+    var myH1 = document.createElement('h1');
+    myH1.textContent = jsonObj['squadName'];
+    header.appendChild(myH1);
+
+    var myPara = document.createElement('p');
+    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+    header.appendChild(myPara);
+}
+
+
+function showHeroes(jsonObj) {
+    var heroes = jsonObj['members'];
+    var sectionarr = [];
+    for (var i = 0; i < heroes.length; i++) {
+        var myArticle = document.createElement('article');
+        var myH2 = document.createElement('h2');
+        var myPara1 = document.createElement('p');
+        var myPara2 = document.createElement('p');
+        var myPara3 = document.createElement('p');
+        var myList = document.createElement('ul');
+
+        myH2.textContent = heroes[i].name;
+        myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+        myPara2.textContent = 'Age: ' + heroes[i].age;
+        myPara3.textContent = 'Superpowers:';
+
+        var superPowers = heroes[i].powers;
+        for (var j = 0; j < superPowers.length; j++) {
+            var listItem = document.createElement('li');
+            listItem.textContent = superPowers[j];
+            myList.appendChild(listItem);
+        }
+
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+        myArticle.appendChild(myList);
+
+        section.appendChild(myArticle);
     }
 }
